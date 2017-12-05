593c3e88d9b89ea6e0adb64926997c2743e345e9
Jan Steinke <j.steinke@emetriq.com>
2017-12-05 16:17:32 +0100

    2017.1.2

--- 2017/1/1.go
+++ 2017/1/1.go
@@ -7,24 +7,29 @@ import (
 
 func main()  {
 	for _, e := range []string{"1122", "1111", "1234", "91212129"} {
-		fmt.Println(sumThatShizzle(e))
+		fmt.Println(sumThatShizzle(e, 1))
 	}
 
 	thingy, err := ioutil.ReadFile("puzzle_input")
 	if err != nil {
 		fmt.Print(err)
 	}
-	fmt.Println(sumThatShizzle(string(thingy)))
+	fmt.Println(sumThatShizzle(string(thingy), 1))
+
+	for _, e := range []string{"1212", "1221", "123425", "123123", "12131415"} {
+		fmt.Println(sumThatShizzle(e, len(e)/2))
+	}
+	fmt.Println(sumThatShizzle(string(thingy), len(string(thingy))/2))
 
 }
 
 
-func sumThatShizzle(input string) int {
+func sumThatShizzle(input string, adder int) int {
 	sum := 0
-	for position, character := range(input) {
-		next := position + 1
-		if next >= len(input) - 1 {
-			next = 0
+	for position, character := range input {
+		next := position + adder
+		if next > len(input) - 1 {
+			next = 0 + (next - len(input))
 		}
 		if input[position] == input[next] {
 			sum += int(character - '0')
