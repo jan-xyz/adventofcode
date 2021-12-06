fd456d3f7fc4ddb3f2dd66205d6542f42961337a
Jan Steinke <jan.steinke@gmail.com>
2021-12-06 10:24:17 +0100

    add 2021/6p2

--- 2021/6/src/lib.rs
+++ 2021/6/src/lib.rs
@@ -1,26 +1,36 @@
-pub fn p1(input: Vec<usize>, days: usize) -> usize {
+use std::collections::HashMap;
+
+pub fn p1(input: Vec<isize>, days: isize) -> usize {
     println!("Initial state: {:?}", input);
 
-    let mut fishes = input;
+    let mut fishes = vec![0, 0, 0, 0, 0, 0, 0, 0, 0];
+    for fish in input {
+        fishes[fish as usize] += 1
+    }
+    println!("{:?}", fishes);
 
     for i in 0..days {
-        println!("After {} days: {:?}", i + 1, fishes);
+        // println!("After {} days: {:?}", i + 1, fishes);
+        println!("day {} has {} fishes", i, fishes.iter().sum::<isize>());
 
-        let mut new_fish = 0;
-        for fish in &mut fishes {
-            if *fish == 0 {
-                new_fish += 1;
-                *fish = 6;
-            } else {
-                *fish -= 1
+        let mut totals_day: Vec<isize> = vec![0, 0, 0, 0, 0, 0, 0, 0, 0];
+        for (days, num_fish) in fishes.iter_mut().enumerate() {
+            if days == 0 {
+                totals_day[8] += *num_fish;
+                totals_day[6] += *num_fish;
+                totals_day[0] -= *num_fish;
+                continue;
             }
+            totals_day[days] -= *num_fish;
+            totals_day[(days - 1)] += *num_fish;
         }
-        for _ in 0..new_fish {
-            fishes.push(8);
+
+        for (days, num_fish) in totals_day.iter().enumerate() {
+            fishes[days] += num_fish;
         }
     }
 
-    fishes.len()
+    fishes.iter().sum::<isize>() as usize
 }
 
 #[cfg(test)]
@@ -31,6 +41,12 @@ mod tests {
     fn test_p1() {
         let input = vec![3, 4, 3, 1, 2];
         assert_eq!(p1(input, 80), 5934);
+    }
+
+    #[test]
+    fn test_p2() {
+        let input = vec![3, 4, 3, 1, 2];
+        assert_eq!(p1(input, 256), 26984457539);
     }
 
     #[test]
@@ -48,6 +64,6 @@ mod tests {
             5, 1, 4, 5, 5, 4, 1, 1, 5, 3, 3, 1, 4, 1, 3, 1, 1, 4, 1, 5, 2, 3, 2, 3, 1, 2, 2, 2, 1,
             1, 5, 1, 4, 5, 2, 4, 2, 2, 3,
         ];
-        assert_eq!(p1(input, 80), 352872);
+        assert_eq!(p1(input, 256), 352872);
     }
 }
